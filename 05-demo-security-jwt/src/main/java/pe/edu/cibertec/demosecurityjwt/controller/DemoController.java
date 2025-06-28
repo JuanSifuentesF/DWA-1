16b6199a561c88c66fe78bb2c7fe235bcff6dd48
Juan Sifuentes Fernandez <89866333+JuanSifuentesF@users.noreply.github.com>
2025-06-28 13:16:06 -0500

    Spring Security completo

--- 05-demo-security-jwt/src/main/java/pe/edu/cibertec/demosecurityjwt/controller/DemoController.java
+++ 05-demo-security-jwt/src/main/java/pe/edu/cibertec/demosecurityjwt/controller/DemoController.java
@@ -2,10 +2,12 @@ package pe.edu.cibertec.demosecurityjwt.controller;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@PreAuthorize("hasRole('ADMIN')") // Esta anotación permite que solo los usuarios con el rol ADMIN puedan acceder a este endpoint
 @RestController
 @RequestMapping("api/v1/demo")
 public class DemoController {
