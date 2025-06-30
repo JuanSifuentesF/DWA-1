7234b5099e9976f1ceac08015003c076d5cdead9
JuanSifuentesF <salemgosu@gmail.com>
2025-06-30 18:27:07 -0500

    test

--- 01-feigclient-basic/src/main/java/pe/edu/cibertec/feigclientbasic/FeigclientBasicApplication.java
+++ 01-feigclient-basic/src/main/java/pe/edu/cibertec/feigclientbasic/FeigclientBasicApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class FeigclientBasicApplication {
-
+  //test
   public static void main(String[] args) {
     SpringApplication.run(FeigclientBasicApplication.class, args);
   }
